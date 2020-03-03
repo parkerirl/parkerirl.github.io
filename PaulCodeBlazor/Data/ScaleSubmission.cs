357eb180a66836bf55e7c7463940c459a0b52ee2
Parker Combs <41436545+parkerirl@users.noreply.github.com>
2020-03-03 10:10:31 -0600

    put new framework

---
+++ PaulCodeBlazor/Data/ScaleSubmission.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaulCodeBlazor.Data
+{
+    public class ScaleSubmission
+    {
+        public string Range_1 { get; set; }
+        public string Range_2 { get; set; }
+        public string Scale_1 { get; set; }
+        public string Scale_2 { get; set; }
+    }
+}
