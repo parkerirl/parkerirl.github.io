8d8b8f9d1ab66e9b0a2085fd0085165fc28ca82a
parkerkcombs <parkerkcombs@gmail.com>
2020-03-03 16:50:00 -0600

    new

--- PaulCodeBlazor/Data/ScaleSubmission.cs
+++ PaulCodeBlazor/Data/ScaleSubmission.cs
@@ -7,8 +7,8 @@ namespace PaulCodeBlazor.Data
 {
     public class ScaleSubmission
     {
-        public string Range_1 { get; set; }
-        public string Range_2 { get; set; }
+        public int Range_1 { get; set; }
+        public int Range_2 { get; set; }
         public string Scale_1 { get; set; }
         public string Scale_2 { get; set; }
     }
